a59c50b8541dbe7a8998c58185d1108bc5c8a4b7
Gabriel <gabrielbittencourt1036@gmail.com>
2025-08-15 19:43:29 -0300

    atv

---
+++ atividades/Aula01/Aula01/Data/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using Aula01.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Aula01.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
